f7991cc9abb38107adf23c400bf9fbfeb4cd0b7e
Marcin <gamer19@poczta.fm>
2020-05-01 11:35:48 +0200

    implement adding restaurants to favourites

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -2,17 +2,22 @@
   <div>
     <p>
       {{this.restaurant}}
+      <button @click="addToFavourites()" id="favourite">dodaj do ulubionych</button>
     </p>
   </div>
 </template>
 
 <script>
+
+import * as firebase from "firebase/app";
+import "firebase/auth";
 export default {
   name: "Restaurant",
   data: function() {
     return {
+      db: firebase.firestore(),
       resID: this.$route.params.restaurantID,
-      restaurant: [],
+      restaurant: null,
       API_key:
         "Bearer fkbp8a45dtXLiROWZHIh6ruBZVTOtm_oNzqlj2NzfDskFb5HMCFjRBEJgkIgJkv-Q0H7IFkT3LWgzDWoNbjsHnZ5WVECz-Fr5lYhR_hYtE_PAPrrwBNeLgtn-MOmXnYx",
     };
@@ -33,5 +38,39 @@ export default {
       .then((json) => (this.restaurant = json))
       .catch((error) => console.log(error));
   },
+  methods: {
+    addToFavourites(){
+      var user = firebase.auth().currentUser
+      var docRef = this.db.collection("favourites").doc(user.uid)
+      var restaurant = this.restaurant
+         if (user) {
+           docRef.get().then(function(doc){
+             if(doc.exists){
+               docRef.update({
+                 restaurants: firebase.firestore.FieldValue.arrayUnion(restaurant)
+               })
+               .then(function(){
+                 alert("dodano")
+               })
+               .catch(function(error){
+                 console.log(error)
+               })
+
+             } else {
+               docRef.set({
+                 restaurants: [restaurant]
+               })
+               .then(function() {
+                  alert("Dodano do ulubionych")
+               })
+               .catch(function(error){
+                 console.log(error)
+               })
+             }
+           })
+         }
+
+    }
+  }
 };
 </script>
