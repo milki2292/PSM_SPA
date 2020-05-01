2357306c9ca173775b60071b6753db815e6f5acc
Marcin <gamer19@poczta.fm>
2020-05-01 14:20:40 +0200

    add second random picture generating API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -159,8 +159,8 @@ export default {
 <style>
 body::after {
   content: "";
-  background: url(.././img/bground-pizza.jpg);
-  opacity: 0.2;
+  background: url(https://source.unsplash.com/random/?food);
+  opacity: 0.5;
   top: 0;
   left: 0;
   bottom: 0;
