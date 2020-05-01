c85812c63f5a51fd1d15fe0c6949eb0c40ac1082
Marcin <gamer19@poczta.fm>
2020-05-01 09:21:14 +0200

    cleaning

--- app/js/displayAvatar.js
+++
@@ -1,8 +0,0 @@
-firebase.auth().onAuthStateChanged(function (user) {
-    storageRef = firebase.storage().ref(user.uid + "/avatar/" + "my_avatar")
-    storageRef.getDownloadURL().then(function (url) {
-                document.getElementById("avatarURL").setAttribute("src", url)
-            }).catch(function (error) {
-                console.log(error)
-            })
-})
