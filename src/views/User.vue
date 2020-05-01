85ad681d9fa774b1abcdb540b62aec7489e90c3d
Marcin <gamer19@poczta.fm>
2020-05-01 14:00:13 +0200

    implement display favourites

--- src/views/User.vue
+++ src/views/User.vue
@@ -5,6 +5,7 @@
         <img id="avatarURL" src="https://img.pngio.com/parent-directory-avatar-2png-avatar-png-256_256.png" width="200"
             height="200">
         <button @click="goManagement">Zarządzaj Kontem</button>
+        <div> {{this.restaurants[0]}} </div>
     </div>
 </template>
 
@@ -13,7 +14,16 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 export default {
     name:"Users",
+    data: function() {
+    return {
+      db: firebase.firestore(),
+      restaurants: [],
+    };
+  },
      created(){
+         var restaurants = this.restaurants
+         var dbRef = this.db.collection("favourites")
+
          firebase.auth().onAuthStateChanged(function (user) {
             const storageRef = firebase.storage().ref(user.uid + "/avatar/" + "my_avatar")
             storageRef.getDownloadURL().then(function (url) {
@@ -21,7 +31,20 @@ export default {
             }).catch(function (error) {
                 console.log(error)
             })
-})
+            dbRef.doc(user.uid).get().then(function(doc) {
+                if (doc.exists) {
+                    doc.data().restaurants.forEach(el => {
+                        restaurants.push(el)
+                    })
+                } else {
+                    console.log("Nie ma ulubionych");
+                }
+                }).catch(function(error) {
+                    console.log("Error getting document:", error);
+            });
+
+        })
+
      },
     methods: {
         goManagement(){
